24988fb7d935c6645239dc1b7c1c882ea89b73e5
liqiang <liqianga@uniontech.com>
2022-11-02 14:49:23 +0800

    fix: the issue of custom-screensaver screen display
     When there is no picture in custom-screensaver's Settings directory, the screensaver should display black
    
    Log: fix the issue of custom-screensaver screen display
    Bug: https://pms.uniontech.com/bug-view-167341.html

--- customscreensaver/deepin-custom-screensaver/translations/deepin-custom-screensaver_zh_CN.ts
+++ customscreensaver/deepin-custom-screensaver/translations/deepin-custom-screensaver_zh_CN.ts
@@ -4,59 +4,67 @@
 <context>
     <name>QObject</name>
     <message>
-        <location filename="../deepin-custom-screensaver_translation.cpp" line="7"/>
+        <location filename="../src/deepin-custom-screensaver_translation.cpp" line="7"/>
         <source>Shuffle</source>
         <translation>随机轮播</translation>
     </message>
     <message>
-        <location filename="../deepin-custom-screensaver_translation.cpp" line="8"/>
+        <location filename="../src/deepin-custom-screensaver_translation.cpp" line="8"/>
         <source>Time interval</source>
         <translation>轮播间隔</translation>
     </message>
     <message>
-        <location filename="../deepin-custom-screensaver_translation.cpp" line="9"/>
+        <location filename="../src/deepin-custom-screensaver_translation.cpp" line="9"/>
         <source>1 minutes</source>
         <translation>1 分钟</translation>
     </message>
     <message>
-        <location filename="../deepin-custom-screensaver_translation.cpp" line="10"/>
+        <location filename="../src/deepin-custom-screensaver_translation.cpp" line="10"/>
         <source>3 minutes</source>
         <translation>3 分钟</translation>
     </message>
     <message>
-        <location filename="../deepin-custom-screensaver_translation.cpp" line="11"/>
+        <location filename="../src/deepin-custom-screensaver_translation.cpp" line="11"/>
         <source>5 minutes</source>
         <translation>5 分钟</translation>
     </message>
     <message>
-        <location filename="../deepin-custom-screensaver_translation.cpp" line="12"/>
+        <location filename="../src/deepin-custom-screensaver_translation.cpp" line="12"/>
         <source>10 minutes</source>
         <translation>10 分钟</translation>
     </message>
     <message>
-        <location filename="../deepin-custom-screensaver_translation.cpp" line="13"/>
+        <location filename="../src/deepin-custom-screensaver_translation.cpp" line="13"/>
         <source>30 minutes</source>
         <translation>30 分钟</translation>
     </message>
     <message>
-        <location filename="../deepin-custom-screensaver_translation.cpp" line="14"/>
+        <location filename="../src/deepin-custom-screensaver_translation.cpp" line="14"/>
         <source>60 minutes</source>
         <translation>60 分钟</translation>
     </message>
     <message>
-        <location filename="../deepin-custom-screensaver_translation.cpp" line="15"/>
+        <location filename="../src/deepin-custom-screensaver_translation.cpp" line="15"/>
         <source>Custom Screensaver</source>
         <translation>自定义屏保</translation>
     </message>
     <message>
-        <location filename="../deepin-custom-screensaver_translation.cpp" line="16"/>
+        <location filename="../src/deepin-custom-screensaver_translation.cpp" line="16"/>
         <source>Path</source>
         <translation>路径选择</translation>
     </message>
     <message>
-        <location filename="../deepin-custom-screensaver_translation.cpp" line="17"/>
+        <location filename="../src/deepin-custom-screensaver_translation.cpp" line="17"/>
         <source>Slideshow</source>
         <translation>轮播设置</translation>
+    </message>
+</context>
+<context>
+    <name>SlideshowScreenSaver</name>
+    <message>
+        <location filename="../src/slideshowscreensaver.cpp" line="106"/>
+        <source>Picture not found</source>
+        <translation>图片不见了</translation>
     </message>
 </context>
 </TS>
