637926d74f2ec6214d0e08b325d01238cac7dcf0
HeXiaodong <hexiaodong@uniontech.com>
2023-08-25 11:44:16 +0800

    feat: ScreenSaver add dconfig
    Log: Add dconfig in screensaver.
    
    Task:

--- customscreensaver/deepin-custom-screensaver/src/slideshowconfig.h
+++ customscreensaver/deepin-custom-screensaver/src/slideshowconfig.h
@@ -8,12 +8,38 @@
 #include <QObject>
 #include <QSettings>
 #include <QScopedPointer>
+#include <DConfig>
+
+DCORE_USE_NAMESPACE
+
+static const char *const kGroupSlideshowPath = "custom_screensaver.path.select_path";
+static const char *const kGroupIntervalTime = "custom_screensaver.slideshow.time_interval";
+static const char *const kGroupShuffle = "custom_screensaver.slideshow.shuffle";
+static const char *const kKeyValue = "value";
+
+static const char *const kDeepinScreenSaver = "deepin-screensaver";
+
+static const int kMaxIntervalTime = 3600;
+static const int kMinIntervalTime = 3;
+static const int kDefaultTime = 10;
+
+static constexpr char kCfgAppId[] { "org.deepin.screensaver" };
+static constexpr char kCfgName[] { "org.deepin.customscreensaver" };
+
+static constexpr char kSlideshowPath[] { "slideshowPath" };
+static constexpr char kIntervalTime[] { "intervalTime" };
+static constexpr char kPlayMode[] { "playMode" };
 
 class SlideshowConfig : public QObject
 {
     Q_OBJECT
 
 public:
+    enum PlayMode {
+        Order = 0,
+        Shuffle = 1
+    };
+
     static SlideshowConfig *instance();
 
     ~SlideshowConfig();
@@ -26,6 +52,7 @@ public:
     bool isShuffle() const;
     void setShuffle(const bool shuffle);
 
+    bool startCustomConfig();
 private:
     QString confPath();
 
@@ -38,6 +65,7 @@ private:
 
 private:
     QScopedPointer<QSettings> m_settings;
+    DConfig *m_dCfg;
 };
 
 #endif   // SLIDESHOWCONFIG_H
